412286adbaf617f5fcedb8d18224801b22503c16
David Peter <david.a.peter@gmail.com>
2011-04-03 02:01:02 -0400

    Hides post link correctly.

--- posts/static/js/main.js
+++ posts/static/js/main.js
@@ -1,11 +1,11 @@
 function toggle_comment(id){
 	var item = $("#" + id);
-
 	if (item.css("display") == "none") {
 		$("#" + id).css("display", "block");
 	} else {
 		$("#" + id).css("display", "none");
 	}
+	$(this).hide();
 }
 /* ensure that CSRF token is sent with AJAX requests */
 $('html').ajaxSend(function(event, xhr, settings) {
